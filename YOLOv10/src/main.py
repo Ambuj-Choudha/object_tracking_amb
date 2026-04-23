480aa1297aa09b04bd9cbc8ce866b8f7dbbef6d8
Ambuj Choudha <ambuj.choudha@rwth-aachen.de>
2026-04-23 15:23:49 +0200

    refactor: main.py for headless behaviour

--- YOLOv10/src/main.py
+++ YOLOv10/src/main.py
@@ -3,42 +3,41 @@ import sys
 import cv2
 
 from detector import YOLOv10Detector
-from common_utils.visualizer import Visualizer
+from utils.draw_detections import Visualizer
 
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(BASE_DIR))
+
+# Constants
+INPUT_FOLDER = "input_images"
+FILE_NAME = "horse.jpg"
+OUTPUT_FOLDER = "output"
+
+MODEL_WEIGHTS = "yolov10m.pt"
+
+DATASET_FOLDER = "dataset"
+DATASET_FILE = os.path.join(PROJECT_ROOT, DATASET_FOLDER, "coco.names")
+ONNX = True
 
 if __name__ == "__main__":
 
-    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    PROJECT_ROOT = os.path.dirname(os.path.dirname(BASE_DIR))
-
-    # Constants
-    INPUT_FOLDER = "input_images"
-    FILE_NAME = "horse.jpg"
-    MODEL_WEIGHTS = 'yolov10m.pt'
-    DATASET_FOLDER = 'dataset'
-    DATASET_FILE = os.path.join(PROJECT_ROOT, DATASET_FOLDER, 'coco.names')
-
-    # get classes for the dataset
     with open(DATASET_FILE, 'r') as file: 
         class_names = file.read().splitlines()
     
-    # read the input image
     img_path = os.path.join(PROJECT_ROOT, INPUT_FOLDER, FILE_NAME)
     img = cv2.imread(img_path)
     if img is None:
         raise FileNotFoundError(f"Could not read image at {img_path}")
     
-    # load trained model
     detector = YOLOv10Detector(MODEL_WEIGHTS)
 
-    # pass the processed image and get inference
     detections = detector.get_detections(img)
 
-    # visualise the output
     visualizer = Visualizer(class_names)
     visualizer.draw_detections(img, detections)
 
-    # show final image
-    cv2.imshow("Detections", img)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    output_path = os.path.join(PROJECT_ROOT, OUTPUT_FOLDER, "detections.jpg")
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    cv2.imwrite(output_path, img)
+    print(f"Detection results saved to {output_path}")

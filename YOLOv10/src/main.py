f7d8f36d94cd417b8137f2ecee3724dc00918e39
Ambuj Choudha <ambuj.choudha@rwth-aachen.de>
2026-04-28 01:56:36 +0200

    added: config file and argparser to main

--- YOLOv10/src/main.py
+++ YOLOv10/src/main.py
@@ -1,46 +1,40 @@
+import argparse
 import cv2
+import config
 from detector import YOLOv10Detector
 from detector_onnx import YOLOv10DetectorONNX
 import os
 from utils.draw_detections import Visualizer
 
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT = os.path.dirname(os.path.dirname(BASE_DIR))
-
-INPUT_FOLDER = "input_images"
-FILE_NAME = "horse.jpg"
-OUTPUT_FOLDER = "output"
-
-PYTORCH_MODEL_WEIGHTS = "yolov10m.pt"
-ONNX_FOLDER = "onnx_model"
-ONNX_MODEL = os.path.join(BASE_DIR, ONNX_FOLDER, "yolov10m.onnx")
-
-DATASET_FOLDER = "dataset"
-DATASET_FILE = os.path.join(PROJECT_ROOT, DATASET_FOLDER, "coco.names")
-ONNX_INFERENCE = True
-
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-image",    required=True,                  help="Input image filename (inside INPUT_FOLDER/)")
+    parser.add_argument("--output-folder",  default=config.OUTPUT_FOLDER,   help="Folder to save results")
+    parser.add_argument("--onnx-inference", action="store_true",            help="To use ONNX runtime")
+    args = parser.parse_args()
 
-    with open(DATASET_FILE, 'r') as file: 
+
+    with open(config.DATASET_FILE, 'r') as file: 
         class_names = file.read().splitlines()
     
-    img_path = os.path.join(PROJECT_ROOT, INPUT_FOLDER, FILE_NAME)
+    img_path = os.path.join(config.INPUT_FOLDER, args.input_image)
     img = cv2.imread(img_path)
     if img is None:
         raise FileNotFoundError(f"Could not read image at {img_path}")
     
-    if ONNX_INFERENCE:
-        detector = YOLOv10DetectorONNX(ONNX_MODEL)
+    if args.onnx_inference:
+        detector = YOLOv10DetectorONNX(config.ONNX_MODEL, config.CONFIDENCE_THRESHOLD)
     else:
-        detector = YOLOv10Detector(PYTORCH_MODEL_WEIGHTS)
+        detector = YOLOv10Detector(config.PYTORCH_MODEL_WEIGHTS, config.CONFIDENCE_THRESHOLD, config.NMS_IOU_THRESHOLD)
 
     detections = detector.get_detections(img)
 
     visualizer = Visualizer(class_names)
     visualizer.draw_detections(img, detections)
 
-    output_path = os.path.join(PROJECT_ROOT, OUTPUT_FOLDER, "detections.jpg")
+    name, ext = os.path.splitext(args.input_image)
+    output_path = os.path.join(args.output_folder, f"{name}_detections{ext}")
+    
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     cv2.imwrite(output_path, img)
     print(f"Detection results saved to {output_path}")

aec8ece19a2d8df172fe61c6b7baaf7820499b7a
Ambuj Choudha <ambuj.choudha@rwth-aachen.de>
2026-04-28 10:10:45 +0200

    removed: pytorch model for simplicity

--- YOLOv10/src/main.py
+++ YOLOv10/src/main.py
@@ -1,7 +1,6 @@
 import argparse
 import cv2
 import config
-from detector import YOLOv10Detector
 from detector_onnx import YOLOv10DetectorONNX
 import os
 from utils.draw_detections import Visualizer
@@ -10,7 +9,6 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--input-image",    required=True,                  help="Input image filename (inside INPUT_FOLDER/)")
     parser.add_argument("--output-folder",  default=config.OUTPUT_FOLDER,   help="Folder to save results")
-    parser.add_argument("--onnx-inference", action="store_true",            help="To use ONNX runtime")
     args = parser.parse_args()
 
 
@@ -22,10 +20,7 @@ if __name__ == "__main__":
     if img is None:
         raise FileNotFoundError(f"Could not read image at {img_path}")
     
-    if args.onnx_inference:
-        detector = YOLOv10DetectorONNX(config.ONNX_MODEL, config.CONFIDENCE_THRESHOLD)
-    else:
-        detector = YOLOv10Detector(config.PYTORCH_MODEL_WEIGHTS, config.CONFIDENCE_THRESHOLD, config.NMS_IOU_THRESHOLD)
+    detector = YOLOv10DetectorONNX(config.ONNX_MODEL, config.CONFIDENCE_THRESHOLD)
 
     detections = detector.get_detections(img)
 
